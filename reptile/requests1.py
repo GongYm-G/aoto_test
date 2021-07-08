e07e86d85e19815d1ff505e43d8aa387b52b558d
Administrator <1163448367@qq.com>
2021-07-08 10:49:49 +0800

    git push

---
+++ reptile/requests1.py
@@ -0,0 +1,26 @@
+import requests
+import re
+
+
+class RQ:
+    def __init__(self, url, values):
+        self.url = url
+        self.UserName = values['UserName']
+        self.Password=values['Password']
+        self.LoginType=values['LoginType']
+
+
+
+
+    # r = requests.post(url, values)
+    # q = r.text
+    # w = re.split(',', q)
+    # print(w)
+    # for i in w:
+    #     print(i)
+
+
+
+url = 'http://10.0.2.62:50500/Login/Login'
+values = {'UserName':'qingypqgly', 'Password':666666, 'LoginType':3}
+r = RQ(url, values)
