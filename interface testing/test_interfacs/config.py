267e99646311cb0701737c45c9503497f0486d29
gong <1163448367@qq.com>
2021-07-26 11:47:55 +0800

    git add

--- interface testing/test_interfacs/config.py
+++ interface testing/test_interfacs/config.py
@@ -7,6 +7,7 @@ class Config:
 
     def __init__(self):
         self.path = 'C:\\Users\\Administrator.RY-20190705FBIQ\\Desktop\\新建文件夹 (2)\\接口基本信息.xls'
+        self.path1 = r"D:\webdriver\接口基本信息.xls"
 
 
     def test_interface(self,url,data,jg):
@@ -24,27 +25,28 @@ class Config:
             return '测试失败', r.text
 
     def open(self):
-        ex = xlrd.open_workbook_xls(self.path)
-        ex1 = ex.sheet_by_index(2)
-        sheet = ex.sheet_names()[2]
+        ex = xlrd.open_workbook_xls(self.path1)
+        ex1 = ex.sheet_by_index(3)
+        sheet = ex.sheet_names()[3]
         cop = copy(ex)
         cop1=cop.get_sheet(sheet)
         values = [ex1.row_values(i) for i in range(1, ex1.nrows)]
         for i in range(len(values)):
-            a = [i for i in values[i][3:14:2]]
-            b = [i for i in values[i][4:15:2]]
+            a = [i for i in values[i][7::2]]
+            b = [i for i in values[i][8::2]]
             di = dict(zip(a, b))
             try:
                 di.pop('')
             except KeyError:
                 pass
-            a = self.test_interface(values[i][2], di,values[i][-3])
-            if '测试通过' == a:
-                cop1.write(i+1, 16, a)
-            else:
-                cop1.write(i+1, 16, a[0])
-                cop1.write(i+1, 17, a[1])
-        cop.save('C:\\Users\\Administrator.RY-20190705FBIQ\\Desktop\\新建文件夹 (2)\\接口基本信息2.xls')
+            print(di)
+        #     a = self.test_interface(values[i][2], di,values[i][-3])
+        #     if '测试通过' == a:
+        #         cop1.write(i+1, 16, a)
+        #     else:
+        #         cop1.write(i+1, 16, a[0])
+        #         cop1.write(i+1, 17, a[1])
+        # cop.save('C:\\Users\\Administrator.RY-20190705FBIQ\\Desktop\\新建文件夹 (2)\\接口基本信息2.xls')
 
 
 
