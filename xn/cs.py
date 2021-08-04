19964eeb348c0475e428cd2afe6af74f20942297
Administrator <1163448367@qq.com>
2021-08-04 17:59:34 +0800

    git push

--- xn/cs.py
+++ xn/cs.py
@@ -1,37 +1,19 @@
-from locust import HttpLocust, task, TaskSet
+from locust import HttpUser, task, TaskSet
+import time,requests
+from locust import HttpUser, task, between
 
 
-# 定义测试类：用户行为
-class UserBehavior(TaskSet):
-    # 指定测试任务
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2.5)
+
     @task
-    def test_login(self):
-        self.client.get("/")
+    def hello_world(self):
+        self.client.post("http://10.0.2.62:50500/Login/Login", json={"UserName":"13700000001", "Password":123456,'LoginType':3})
 
 
-class WebSiteUser(HttpLocust):
-    host="https://www.baidu.com/"
-    task_set=UserBehavior
-    min_wait=2000
-    max_wait=5000
-
-
-##import time
-##from locust import HttpUser, task, between
-##
-##class QuickstartUser(HttpUser):
-##    wait_time = between(1, 2.5)
-##
-##    @task
-##    def hello_world(self):
-##        self.client.get("/hello")
-##        self.client.get("/world")
-##
-##    @task(3)
-##    def view_items(self):
-##        for item_id in range(10):
-##            self.client.get(f"/item?id={item_id}", name="/item")
-##            time.sleep(1)
-##
-##    def on_start(self):
-##        self.client.post("/login", json={"username":"foo", "password":"bar"})
+    def on_start(self):
+        a=self.client.post("http://10.0.2.62:50500/Login/Login", json={"UserName":"13700000001", "Password":123456,'LoginType':3}).text
+        if a.find('金太阳第一中学')>0:
+            print(True)
+        else:
+            print(False)
